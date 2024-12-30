17b0891123f8c33795b9746aad8601a5eb224ff8
Praveen Dsouza <dsouzapraveen24@gmail.com>
2024-12-30 16:21:42 +0530

    server form with useActionState hook

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,19 +1,62 @@
-'use server'
+"use server";
 
 import { redirect } from "next/navigation";
-import { db } from "@/db"
+import { db } from "@/db";
 
 export async function editSnippet(id: number, code: string) {
-    await db.snippet.update({
-        where: { id },
-        data: { code }
-    });
-    redirect(`/snippets/${id}`);
+  await db.snippet.update({
+    where: { id },
+    data: { code },
+  });
+  redirect(`/snippets/${id}`);
 }
 
 export async function deleteSnippet(id: number) {
-    await db.snippet.delete({
-        where: { id }
+  await db.snippet.delete({
+    where: { id },
+  });
+  redirect("/");
+}
+
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check the user's inputs and make sure they are valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+
+    if (typeof code !== "string" || code.length < 3) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: { title, code },
     });
-    redirect('/')
+
+    // throw new Error("Failed to save to database.");
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+
+  // Redirect the user back to the root route
+  redirect("/");
 }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,26 +1,19 @@
-import { redirect } from "next/navigation";
-import { db } from "@/db";
+'use client';
+
+import { useActionState, startTransition } from "react";
+import { createSnippet } from "@/actions";
 
 export default function SnippetCreatePage() {
-    async function createSnippet(formData: FormData) {
-        // There needs to be a server action!
-        'use server';
+    const [formState, action] = useActionState(createSnippet, { message: '' });
 
-        // Check the user's inputs and make sure they are valid
-        const title = formData.get('title') as string;
-        const code = formData.get('code') as string;
-
-        // Create a new record in the database
-        await db.snippet.create({
-            data: { title, code }
-        })
-
-        // Redirect the user back to the root route
-        redirect('/');
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) =>{
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        startTransition(() => action(formData))
     }
-
+  
     return (
-        <form action={createSnippet}>
+        <form onSubmit={handleSubmit}>
             <h3 className='font-bold m-3'>Create a Snippet</h3>
             <div className="flex flex-col gap-4">
                 <div className="flex gap-4">
@@ -31,6 +24,7 @@ export default function SnippetCreatePage() {
                     <label className="w-12" htmlFor="code">Code</label>
                     <textarea name="code" className="border rounded p-2 w-full" id="code" />
                 </div>
+                {formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div>: null}
                 <button type="submit" className="rounded p-2 bg-blue-200">Create</button>
             </div>
         </form>
